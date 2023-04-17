2dfcfda193fd14246df4322c540cebbb01341c21
Brad Rucker <bruck63@gmail.com>
2023-04-17 14:45:57 -0500

    Week 1 Challenge Complete.

---
+++ Week1_Challenge.js
@@ -0,0 +1,11 @@
+//Week 1 Coding Challenge
+//Area of a Triangle:  Write a function that takes the base and height of a triangle and return its area.
+
+function triArea(base, height) {
+	return (base * height)/2;
+}
+
+//Test Answers
+console.log(triArea(3,2));
+console.log(triArea(7,4));
+console.log(triArea(10,10));
